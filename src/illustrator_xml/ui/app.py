c97eba2c8ed4762f38605b579ae10a3312686bb6
AmZaDin14 <amrireza97@gmail.com>
2025-06-27 01:23:47 +0700

    Refactor project structure: remove old files and add CSV to XML conversion functionality with GUI

--- src/illustrator_xml/ui/app.py
+++ src/illustrator_xml/ui/app.py
@@ -1,7 +1,7 @@
 import os
 import tkinter as tk
 from tkinter import filedialog, messagebox
-from core.converter import csv_to_xml
+from illustrator_xml.core.converter import csv_to_xml
 
 class CsvToXmlUI(tk.Tk):
     def __init__(self):
